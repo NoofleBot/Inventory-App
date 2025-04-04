6c7676333dd939204ffc0b5e2d343eef038399af
NoofleBot <nooflebot@gmail.com>
2025-04-04 00:24:31 -0700

    Added item viewing functionality based on logged in user, view item button routes to specific item, logged in users can see & use create new item button. More CSS updates / additions to make the site not look crusty.

--- frontend/src/JSX/Home.jsx
+++ frontend/src/JSX/Home.jsx
@@ -1,11 +1,59 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../CSS/Home.css'
 
 function Home({ loggedInUserId }) {
+  const [items, setItems] = useState([])
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch('http://localhost:5172/items')
+        const data = await response.json()
+        if (loggedInUserId) {
+          const userItems = data.filter((item) => item.user_id === loggedInUserId)
+          setItems(userItems)
+          if (userItems.length === 0) {
+            setMessage('No items found for your account, get started by adding a new item or log out to see all items.')
+          }
+        } else {
+          setItems(data)
+          setMessage('')
+        }
+      } catch (error) {
+        setMessage('Error fetching items from the server.')
+      }
+    }
+    fetchItems()
+  }, [loggedInUserId])
+
   return (
     <>
       <h1>Current Inventory:</h1>
-      <p>Logged in as User ID: {loggedInUserId}</p>
+      {loggedInUserId && (
+        <div className="createitembutton">
+          <Link to="/createNewItem">Create New Item</Link>
+        </div>
+      )}
+      <p><b>{message}</b></p>
+      {items.map((item) => (
+        <div>
+        <div key={item.id} className="itemcard">
+          <h3><b>{item.item_name}</b></h3>
+          <p>
+          {item.description.length > 100
+             ? `${item.description.slice(0, 100)}...`
+             : item.description}
+          </p>
+          <p>Quantity: {item.quantity}</p>
+          <div className="viewitembutton">
+            <Link to={`/item/${item.id}`}>View {item.item_name}</Link>
+          </div>
+        </div>
+        <br></br> {/* Lazy non-CSS way of seperating item cards */ }
+      </div>
+      ))}
     </>
   )
 }
